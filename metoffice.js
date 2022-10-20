60d0a3b5c015f6b77d351cdf2f1e1d13163c6b9f
AriDemSoftwire <arina.demidova@softwire.com>
2022-10-20 14:27:43 +0100

    Added the correct url and a promise

--- metoffice.js
+++ metoffice.js
@@ -1,4 +1,22 @@
-fetch('http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/310042?res=3hourly&key=866ba1d3-9f94-4c30-8c79-121f956442cc')           //api for the get request
-  .then(response => response.json())
-  .then(data => console.log(data));
-  
+
+let parsedData = fetch('http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=dd89d98f-dc78-4ec5-9bc4-ec5ba68ee17b')           //api for the get request
+.then(response => response.json())
+.then((data) => {
+  //console.log(data);
+  return data
+})
+
+const promise = new Promise((resolve, reject) => {
+setTimeout(() => resolve(console.log(parsedData)), 2000);
+});
+
+for (i)
+
+//console.log(parsedData);
+// .then(data => parsedData.push(data))
+//  .then(console.log(parsedData))
+
+/* fetch('http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/310042?res=3hourly&key=866ba1d3-9f94-4c30-8c79-121f956442cc')           //api for the get request
+.then(response => response.json())
+.then(data => console.log(data));
+*/
